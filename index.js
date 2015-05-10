1e6646a0eb9aeca7b3e20004ea1ee2f935b6ff0f
switer <guankaishe@gmail.com>
2015-05-10 11:26:02 +0800

    compelete, but it's not a forever cache

--- index.js
+++ index.js
@@ -1,44 +1,75 @@
 'use strict';
 
-var DEFAULT_MAX_AGE = 5 * 1000 // 3s
-var DEFAULT_STALE_PERCENt = 
-function Cache() {
+var DEFAULT_MAX = 1000
+var DEFAULT_MAX_AGE = 5 * 1000 // ms, default 5s
+var DEFAULT_STALE_PERCENT = 0.4
+
+function Cache(opts) {
     this._caches = {}
-    this._settings = {
-        max: 1000, // Number
+    this._settings = _merge({
+        max: DEFAULT_MAX, // Number
         maxAge: DEFAULT_MAX_AGE, // Number/ms
-        stalePercent: 0.4 // how many the percentage of caches nead to remove in batch 
-    }
+        stalePercent: DEFAULT_STALE_PERCENT, // how many the percentage of caches nead to remove in batch 
+        imutable: false // whether using new references for object value
+    }, opts)
 }
 
 var proto = Cache.prototype
-proto.setting = function(st) {
-    _merge(this._settings, st)
-    return this
-}
 proto.set = function(key, value, maxAge) {
     if (Object.keys(this._caches).length >= this._settings.max) {
-        this.removeExpired()
+        this.free()
     }
     this._caches[key] = {
-        data: value
-        expire: maxAge == 0 ? 0 : ((maxAge || DEFAULT_MAX_AGE) + new Date)
+        data: _immutable(value, this._settings.imutable),
+        expire: (maxAge || DEFAULT_MAX_AGE) + new Date
     }
 }
 proto.get = function(key) {
-    var data = this._caches[key]
-    if (!data) return undefined
-    var expire = data.expire
-    return {
-        data: _immutable(data.data, this._settings.imutable),
-        //Boolean
-        expired: expire == 0 ? false: (new Date >= expire) 
-    }
-}
+    var caches = this._caches
+    var item = this._caches[key]
+    if (!item) return undefined
 
-proto.removeExpired = function () {
+    var expire = item.expire
+    if (new Date >= expire) {
+        // delete caches[key]
+        return undefined
+    }
+    return _immutable(item.data, this._settings.imutable)
+}
+proto.free = function () {
     var keys = Object.keys(this._caches)
-    // var removeTotal = Math.ceil(this.)
+    var freeSize = Math.ceil(this._settings.max*this._settings.stalePercent)
+    var usedSize = this._settings.max - freeSize
+    var caches = this.caches
+    var time = +new Date
+    var ckeys = []
+    // remove expire
+    keys.forEach(function (k) {
+        var item = caches[k]
+        // exist and not expired
+        if (item.expire > time) {
+            ckeys.push(item)
+        }
+    })
+    if (ckeys.length > usedSize) {
+        // clear those near expired
+        // From new to old
+        ckeys.sort(function (a, b) {
+            a = caches[a].expire
+            b = caches[b].expire
+
+            if (a > b) return -1
+            else if (a < b) return 1
+            else return 0
+        })
+        ckeys = ckeys.slice(0, usedSize)
+    }
+    var newCaches = {}
+    ckeys.forEach(function (k) {
+        newCaches[k] = caches[k]
+    })
+    this._caches = newCaches
+    return this
 }
 
 function _immutable (data, bool) {
